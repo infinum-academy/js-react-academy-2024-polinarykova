01c80f8a3e3bf4c58121ef9e655ddde7439cdf52
Polina <polina.rykova.hr@gmail.com>
2024-07-02 21:16:03 +0200

    add remove button to the reviews

--- Homework-1/index.js
+++ Homework-1/index.js
@@ -2,20 +2,28 @@ function loadReviews() {
   if (!localStorage.getItem("reviews")) localStorage.setItem("reviews", []);
   const reviews = JSON.parse(localStorage.getItem("reviews"));
   document.getElementsByClassName("reviews")[0].innerHTML = "";
-  for (review of reviews) {
+  let index = 0;
+  reviews.forEach((review, index) => {
     const description = document.createElement("p");
     description.innerText = review.description;
 
     const rating = document.createElement("p");
     rating.innerText = review.rating + "/5";
 
+    const removeButton = document.createElement("button");
+    removeButton.id = "button";
+    removeButton.onclick = () => removeReview(index);
+    removeButton.innerText = "Remove";
+
     const reviewEl = document.createElement("div");
     reviewEl.classList = ["review"];
+
     reviewEl.appendChild(description);
     reviewEl.appendChild(rating);
+    reviewEl.appendChild(removeButton);
 
     document.getElementsByClassName("reviews")[0].appendChild(reviewEl);
-  }
+  });
 }
 
 function handleSubmit(event) {
@@ -35,5 +43,16 @@ function handleSubmit(event) {
   reviews.push(newReview);
   localStorage.setItem("reviews", JSON.stringify(reviews));
 
+  loadReviews();
+}
+
+function removeReview(index) {
+  const reviews = localStorage.getItem("reviews")
+    ? JSON.parse(localStorage.getItem("reviews"))
+    : [];
+  console.log(index);
+  reviews.splice(index, 1);
+  console.log(reviews);
+  localStorage.setItem("reviews", JSON.stringify(reviews));
   loadReviews();
 }

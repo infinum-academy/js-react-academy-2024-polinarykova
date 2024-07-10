ad949dc35b27a8a21928fafe20d7fd7c9fe460f3
Polina <polina.rykova.hr@gmail.com>
2024-07-11 00:23:23 +0200

    add redirecting from root page

--- tv-shows-app/src/app/page.tsx
+++ tv-shows-app/src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import React from "react";
-
-import ShowList from "@/components/shared/ShowList/ShowList";
 
 export default function Home() {
-  const mockList = { shows: [] };
-  return <ShowList showList={mockList}></ShowList>;
+  window.location.href = "/all-shows";
 }

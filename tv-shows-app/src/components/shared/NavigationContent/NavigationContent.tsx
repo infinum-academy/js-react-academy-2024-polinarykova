fa50bec4b960b041b73514c80cac0843187ace71
Polina <polina.rykova.hr@gmail.com>
2024-07-21 14:20:49 +0200

    create Drawer component for mobile navigation

---
+++ tv-shows-app/src/components/shared/NavigationContent/NavigationContent.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { Flex, Text } from "@chakra-ui/react";
+import { FiLogOut } from "react-icons/fi";
+import NavigationLink from "../NavigationLink/NavigationLink";
+import useUserSWR from "@/hooks/useUserSWR";
+import { usePathname } from "next/navigation";
+
+export default function NavigationContent() {
+  const pathname = usePathname();
+
+  const links = [
+    { name: "All shows", path: "/shows" },
+    { name: "Top rated", path: "/top-rated" },
+    { name: "My profile", path: "/my-profile" },
+  ];
+
+  const { mutate } = useUserSWR();
+
+  function handleLogOut() {
+    localStorage.removeItem("headers");
+    mutate(null, { revalidate: false });
+  }
+
+  return (
+    <Flex direction="column" padding={5} height="90vh">
+      <Flex direction="column" flex="1" textStyle="subtitle.regular">
+        {links.map((link, index) => (
+          <NavigationLink
+            key={index}
+            name={link.name}
+            path={link.path}
+            currCategory={pathname || ""}
+          />
+        ))}
+      </Flex>
+
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        gap={3}
+        onClick={handleLogOut}
+        cursor="pointer"
+      >
+        <Text textStyle="smallCaption.regular" letterSpacing="wide">
+          Log out
+        </Text>
+        <FiLogOut />
+      </Flex>
+    </Flex>
+  );
+}
